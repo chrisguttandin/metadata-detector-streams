ba36a15b51df83c58af6c8b0a965be13aa7b0332
chrisguttandin <chrisguttandin@media-codings.com>
2017-02-26 04:30:25 +0100

    run tslint via command line

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,5 +1,8 @@
 module.exports = {
     build: {
         cmd: 'tsc -p src/tsconfig.json'
+    },
+    lint: {
+        cmd: 'tslint -c config/tslint/src.json --project src/tsconfig.json --type-check src/**/*.ts'
     }
 };
